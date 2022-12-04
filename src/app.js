8b1f1c9b634511f896eccd71e75057eb60e98f1e
Danh Pham 000 <danhkid000@gmail.com>
2022-12-04 18:57:43 +0700

    add Security Web

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ require('dotenv').config({ path: 'src/config/.env' });
 // Import everything
 const globalErrorHandle = require('./middlewares/handlerError');
 
+// Security Web
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
+
 // Router import
 const productRouter = require('./routes/productRoute');
 const authRouter = require('./routes/authRoute');
@@ -29,6 +36,25 @@ app.use(express.json({ limit: '10kb' }));
 
 // Cookie parser
 app.use(cookieParser());
+
+// Tránh SQL injection
+app.use(mongoSanitize());
+
+// Set Dns và giấu ip, lọc header
+app.use(helmet());
+
+// Tránh chèn dữ liệu có code html js
+app.use(xss());
+
+// Lọc params
+app.use(hpp());
+
+// Rate limit request with 1 IP
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'src/public')));
